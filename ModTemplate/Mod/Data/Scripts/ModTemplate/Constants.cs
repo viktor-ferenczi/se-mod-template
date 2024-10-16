17495a109e038b9abd09c5a802df41bfb88d8ab9
Viktor Ferenczi <viktor@ferenczi.eu>
2024-10-16 14:40:36 +0200

    Fixed methods in `Constants`

--- ModTemplate/Mod/Data/Scripts/ModTemplate/Constants.cs
+++ ModTemplate/Mod/Data/Scripts/ModTemplate/Constants.cs
@@ -6,10 +6,10 @@ namespace ModTemplate
     public class Constants
     {
         public static Guid ModGuid = new Guid("F7EA06E6-FC5B-4777-AF67-D3E8BB9D24D2");
+        public static bool IsClient => !MyAPIGateway.Session.IsServer;
         public static bool IsServer => MyAPIGateway.Session.IsServer;
-        public static bool IsDedicated => IsServer && MyAPIGateway.Utilities.IsDedicated;
-        public static bool IsPlayer => !IsDedicated;
-        public static bool IsClient => !IsServer;
-        public static bool HasCreativeRights => MyAPIGateway.Session.HasCreativeRights;
+        public static bool IsDedicated => MyAPIGateway.Session.IsServer && MyAPIGateway.Utilities.IsDedicated;
+        public static bool HasCreativeRights => MyAPIGateway.Session.CreativeMode || MyAPIGateway.Session.HasCreativeRights;
+        public static long PlayerIdentityId => MyAPIGateway.Session.Player.IdentityId;
     }
 }
